8312c27ab1b036afa76692e2148c6dad765d6be7
hungtvu113 <tranhunggit@gmail.com>
2025-01-08 13:57:44 +0700

    updl

--- connecting.php
+++ connecting.php
@@ -1,7 +1,7 @@
 <?php
-$servername = "SG-doantt-11256-mysql-master.servers.mongodirector.com";
+$servername = "SG-doantt5-11422-mysql-master.servers.mongodirector.com";
 $username = "sgroot";  // Tên đăng nhập mặc định của XAMPP
-$password = "6b&RQ4vAZ5ZcXvFI";      // Mật khẩu mặc định để trống
+$password = "7uuTV@RKRr3XdjXC";      // Mật khẩu mặc định để trống
 $database = "doan"; // Thay đổi tên database của bạn
 
 // Tạo kết nối
